21995856fa7b1fd3f1b2c061b29ffa1b2bfa0f53
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-01-25 22:40:50 -0800

    Animates conways game of life

--- conway.js
+++ conway.js
@@ -2,11 +2,17 @@
 var settings = {
   width: 500,
   height: 500,
-  xCellCount: 50, 
-  yCellCount: 50,
+  xCellCount: 30, 
+  yCellCount: 30,
   lifeCount: 400,
-  lifeData: []
+  lifeData: [],
+  stepInterval: 150
 };
+
+$(document).ready(function() {
+  setupBoard();
+  animate();
+})
 
 function setupBoard() {
   d3.select('.board')
@@ -21,17 +27,66 @@ function setupBoard() {
       settings.lifeData[i] = 1;
     }
   }
-  //setup cells as data coordinates
   var cellWidth = settings.width / settings.xCellCount;
   var cellHeight = settings.height / settings.yCellCount;
-  var life = d3.select('.board')
-               .selectAll('rect')
-               .data(settings.lifeData);
-  life.enter()
-      .append('rect')
-      .attr('class','cell')
-      .attr('width', cellWidth)
-      .attr('height', cellHeight)
+  d3.select('.board')
+    .selectAll('rect')    
+    .data(settings.lifeData)
+    .enter()
+    .append('rect')
+    .attr('class','cell')
+    .attr('width', cellWidth)
+    .attr('height', cellHeight)
+    .attr('ry', 2)
+    .attr('rx', 2);
+  placeOnBoard();
+}
+
+function animate() {
+  setInterval(function() {
+    var newData = []
+    var lifeData = settings.lifeData;
+    var xCellCount = settings.xCellCount;
+    for(var i = 0; i < lifeData.length; i++) {
+      //check conditions for life
+      //create array of neighbors
+      var neighbors = [lifeData[i+1], lifeData[i-1], lifeData[i + xCellCount], 
+        lifeData[i - xCellCount], lifeData[i + xCellCount + 1], 
+        lifeData[i + xCellCount - 1], lifeData[i - xCellCount + 1], lifeData[i - xCellCount - 1]];
+      var lifeCount = _.reduce(neighbors, function(a, b) {
+        return a + b;
+      })
+      if(lifeData[i]) {
+        //start alive
+        if(lifeCount === 2 || lifeCount === 3) {
+          newData[i] = 1;
+        }
+        else {
+          newData[i] = 0;
+        }
+      }
+      else {
+        //start dead
+        if(lifeCount === 3) {
+          newData[i] = 1;
+        }
+        else {
+          newData[i] = 0;
+        }
+      }
+    }
+    settings.lifeData = newData;
+    placeOnBoard();
+  }, settings.stepInterval);
+}
+
+function placeOnBoard() {
+  var cellWidth = settings.width / settings.xCellCount;
+  var cellHeight = settings.height / settings.yCellCount; 
+  d3.selectAll('rect')
+      .data(settings.lifeData)
+      .transition()
+      .duration(settings.stepInterval * 0.7)
       .attr('x', function(d,i) {
         return (i % settings.xCellCount) * cellWidth;
       })
@@ -42,11 +97,4 @@ function setupBoard() {
         if(d) {return 'blue';}
         else {return 'white';}
       });
-  //choose cells to be alive to start
-  //draw lives as rects
 }
-
-$(document).ready(function() {
-  setupBoard();
-  // animate();
-})
